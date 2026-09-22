415864eae94b36019c3eb332dab8a2fec6e61eb7
seyedmahdim4-beep <seyedmahdim4@gmail.com>
2026-09-22 14:06:54 +0330

    repairing images url

--- src/data/BlogsData.js
+++ src/data/BlogsData.js
@@ -2,7 +2,7 @@ export const BlogsData=[
     {
         id:"1",
         tittle:"طرز تهیه قهوه دمی با دستگاه اروپرس",
-        img:"../image/blogs/blog-1.png",
+        img:"/coffee/image/blogs/blog-1.png",
         day:"10",
         month:"مرداد",
         year:"1405",
@@ -10,7 +10,7 @@ export const BlogsData=[
     {
         id:"2",
         tittle:"یک نوشیدنی هیجان انگیز و پرکالری برای شروع روز",
-        img:"../image/blogs/blog-2.png",
+        img:"/coffee/image/blogs/blog-2.png",
         day:"6",
         month:"مرداد",
         year:"1405",
@@ -18,7 +18,7 @@ export const BlogsData=[
     {
         id:"3",
         tittle:" طرز تهیه یک فنجان کافه زینو برزیلی ",
-        img:"../image/blogs/blog-3.png",
+        img:"/coffee/image/blogs/blog-3.png",
         day:"31",
         month:"تیر",
         year:"1405",
@@ -26,7 +26,7 @@ export const BlogsData=[
     {
         id:"4",
         tittle:"طرز تهیه قهوه دالگونا به بهترین روش",
-        img:"../image/blogs/blog-4.png",
+        img:"/coffee/image/blogs/blog-4.png",
         day:"6",
         month:"خرداد",
         year:"1405",
--- src/data/ProductsData.js
+++ src/data/ProductsData.js
@@ -2,7 +2,7 @@ export const productsData = [
   {
     id: "1",
     name: "قهوه ترک بن مانو 250 گرمی",
-    img: "/image/products/p1.png",
+    img: "/coffee/image/products/p1.png",
     off: "12%",
     disCount: "700,000",
     price: "616,000",
@@ -12,7 +12,7 @@ export const productsData = [
   {
     id: "2",
     name: "قهوه مانوکا بن مانو 250 گرمی",
-    img: "/image/products/p2.png",
+    img: "/coffee/image/products/p2.png",
     off: "",
     disCount: "",
     price: "890,000",
@@ -22,7 +22,7 @@ export const productsData = [
   {
     id: "3",
     name: "قهوه پریسکا بن مانو 250 گرمی",
-    img: "/image/products/p3.png",
+    img: "/coffee/image/products/p3.png",
     off: "",
     disCount: "",
     price: "",
@@ -32,7 +32,7 @@ export const productsData = [
   {
     id: "4",
     name: "قهوه آرتیمان بن مانو مقدار 250 گرمی",
-    img: "/image/products/p4.png",
+    img: "/coffee/image/products/p4.png",
     off: "",
     disCount: "",
     price: "1,457,700",
@@ -42,7 +42,7 @@ export const productsData = [
   {
     id: "5",
     name: "قهوه بن مانو نورسکا 9صبح ، بسته 6عددی",
-    img: "/image/products/p5.png",
+    img: "/coffee/image/products/p5.png",
     off: "10%",
     disCount: "1,197,000",
     price: "1,017,000",
@@ -52,7 +52,7 @@ export const productsData = [
   {
     id: "6",
     name: "قهوه بن مانو نورسکا 6عصر ، بسته 6عددی",
-    img: "/image/products/p6.png",
+    img: "/coffee/image/products/p6.png",
     off: "",
     disCount: "",
     price: "280,000",
@@ -62,7 +62,7 @@ export const productsData = [
   {
     id: "7",
     name: "قهوه بن مانو نورسکا 8شب ، بسته 6عددی",
-    img: "/image/products/p7.png",
+    img: "/coffee/image/products/p7.png",
     off: "",
     disCount: "",
     price: "292,000",
@@ -72,7 +72,7 @@ export const productsData = [
   {
     id: "8",
     name: "قهوه بن مانو نورسکا 2ظهر ، بسته 6عددی",
-    img: "/image/products/p8.png",
+    img: "/coffee/image/products/p8.png",
     off: "15%",
     disCount: "325,000",
     price: "277,000",
